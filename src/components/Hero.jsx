7515c0251c0bcdd3499fe5923a094681afa6dc3f
Reveill3 <38196983+Reveill3@users.noreply.github.com>
2023-01-25 17:40:21 +0000

    Added nav animations and links.
    - Hero section started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    
-
     <section className='heroBackground bg-center bg-cover z-0'>
-
-    <div className='w-full h-[860px] bg-transparent z-10'>
-        Hero
-    </div>
+      <div className='flex flex-col justify-center text-center w-full h-[1080px] bg-black/60 z-10'>
+        <h1 className='title text-white '>Maximizing your revenue with web3</h1>
+      </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,38 +3,40 @@ import { motion } from 'framer-motion';
 
 const NavBar = () => {
   return (
-    <div className='fixed top-0 left-0 w-full flex bg-black/40 h-16 z-10 backdrop-blur-md'>
-      <div className='w-3.75 text-black p-2 flex items-center'>
+    <div className='fixed top-0 left-0 w-full flex bg-black/60 h-16 z-10 backdrop-blur-md'>
+      <div className='p-4 text-black flex items-center w-1/6'>
         <img
-          src='https://picsum.photos/seed/web3/50/50'
+          width='125px'
+          height='125px'
+          src='https://res.cloudinary.com/dgplbptdj/image/upload/v1674663826/Round%20Table/RT_logo_dx3hce.png'
           className='rounded-full'
         />
       </div>
       <div className='flex w-3/4 decoration-none text-white items-center gap-5 text-black justify-around'>
           <motion.a
-            className='p-3 rounded-md border-transparent'
+            className='p-3  rounded-md border-transparent w-[100px] text-center'
             href='/'
             whileHover={{
               'background-color': 'rgba(0, 0, 0, 0.2)',
-              border: '1px solid rgba(255, 255, 255, 0.2)',
+              border: '1px solid rgba(255, 255, 255, 0.4)',
               transition: { duration: 0.4, ease: 'easeIn' },
             }}
           >Home</motion.a>
           <motion.a
-            className='p-3 rounded-md border-transparent'
+            className='p-3 rounded-md border-transparent w-[100px] text-center'
             href='/'
             whileHover={{
               'background-color': 'rgba(0, 0, 0, 0.2)',
-              border: '1px solid rgba(255, 255, 255, 0.2)',
+              border: '1px solid rgba(255, 255, 255, 0.4)',
               transition: { duration: 0.4, ease: 'easeIn' },
             }}
           >About</motion.a>
           <motion.a
-            className='p-3 rounded-md border-transparent border-1'
+            className='p-3 rounded-md border-transparent w-[100px] text-center'
             href='/'
             whileHover={{
               'background-color': 'rgba(0, 0, 0, 0.2)',
-              border: '1px solid rgba(255, 255, 255, 0.2)',
+              border: '1px solid rgba(255, 255, 255, 0.4)',
               transition: { duration: 0.4, ease: 'easeIn' },
             }}
           >Projects</motion.a>
